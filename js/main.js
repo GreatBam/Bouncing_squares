888b14f10ab927b51804d0f3fe89b8aa149dcaa3
Jonathan <jonathan.gabioud@hotmail.com>
2022-10-26 00:37:14 +0200

    Ball collision

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function colorRNG() {
 
 function clear () {
     ctx.beginPath();
-    ctx.rect(0, 0, 500, 500);
+    ctx.rect(0, 0, 1200, 700);
     ctx.fillStyle = "white";
     ctx.strokeStyle = "black";
     ctx.fill();
@@ -41,30 +41,54 @@ function player1Move(e) {
 
 function player2Move(e) {
     switch(e.keyCode) {
-        case 65:
-        player2.moveLeft()
-        break;
+        // case 65:
+        // player2.moveLeft()
+        // break;
         case 87:
         player2.moveUp()
         break;
-        case 68:
-        player2.moveRight()
-        break;
+        // case 68:
+        // player2.moveRight()
+        // break;
         case 83:
         player2.moveDown()
         break;  
     }   
 }
-    
-const player1 = new Square(ctx, 480, 480, colorRNG())
-const player2 = new Square(ctx, 10, 10, colorRNG())
-const ball = new Square(ctx, 40, 40, colorRNG())
 
-const squares = [
-    new Square(ctx, 30, 30, colorRNG()),
-    new Square(ctx, 30, 30, colorRNG()),
-    new Square(ctx, 30, 30, colorRNG())
-]
+function ballCollision() {
+    if((ball.x + ball.w) >= player1.x &&
+        (ball.y + ball.h) >= player1.y &&
+        ball.x <= (player1.x + player1.w) &&
+        ball.y <= (player1.y + player1.h))
+        ball.directionx = ball.directionx * -1;
+    if((ball.y + ball.h) >= player1.y &&
+        (ball.x + ball.h) >= player1.x &&
+        ball.y <= (player1.y + player1.h) &&
+        ball.x <= (player1.x + player1.w))
+        ball.directiony = ball.directiony * -1;
+    if((ball.x + ball.w) >= player2.x &&
+        (ball.y + ball.h) >= player2.y &&
+        ball.x <= (player2.x + player2.w) &&
+        ball.y <= (player2.y + player2.h))
+        ball.directionx = ball.directionx * -1;
+    if((ball.y + ball.h) >= player2.y &&
+        (ball.x + ball.h) >= player2.x &&
+        ball.y <= (player2.y + player2.h) &&
+        ball.x <= (player2.x + player2.w))
+        ball.directiony = ball.directiony * -1;
+}
+    
+const player1 = new Square(ctx, 1180, 680, 40, 150, "red")
+const player2 = new Square(ctx, 20, 20, 40, 150, "blue")
+const ball = new Square(ctx, 600, 350, 50, 50, colorRNG())
+
+// create multiple squares at once
+// const squares = [
+//     new Square(ctx, 30, 30, 50, 50, colorRNG()),
+//     new Square(ctx, 30, 30, 50, 50, colorRNG()),
+//     new Square(ctx, 30, 30, 50, 50, colorRNG())
+// ]
             
 setInterval(() => {
     clear();
@@ -77,9 +101,12 @@ setInterval(() => {
     player1.playerBorderDetection(c.width, c.height);
     player2.playerBorderDetection(c.width, c.height);
     ball.ballBorderDetection(c.width, c.height);
-    for(let square of squares) {
-        square.draw();
-        square.move();
-        square.ballBorderDetection(c.width, c.height) 
-    }
+    ballCollision();
+
+    // multiple squares for loop
+    // for(let square of squares) {
+    //     square.draw();
+    //     square.move();
+    //     square.ballBorderDetection(c.width, c.height) 
+    // }
 }, 1000/60);
--- js/square.js
+++ js/square.js
@@ -1,11 +1,12 @@
 class Square {
-    constructor(ctx, x, y, fill) {
+    constructor(ctx, x, y, w, h, fill) {
         this.ctx = ctx;
         this.x = x;
         this.y = y;
-        this.w = 50;
-        this.directionx = Math.floor(Math.random()*5)+1;
-        this.directiony = Math.floor(Math.random()*5)+1;
+        this.w = w;
+        this.h = h;
+        this.directionx = 5;
+        this.directiony = 5;
         this.fill = fill;
     }
 
@@ -30,20 +31,20 @@ class Square {
     playerBorderDetection(w, h) {
         if(this.x + this.w >= w) this.x -= 10;
         if(this.x <= 0) this.x += 10;
-        if(this.y + this.w >= h) this.y = this.y -= 10;
+        if(this.y + this.h >= h) this.y = this.y -= 10;
         if(this.y <= 0) this.y += 10;
     }
 
     ballBorderDetection(w, h) {
         if(this.x + this.w >= w) this.directionx = -this.directionx;
         if(this.x <= 0) this.directionx = -this.directionx;
-        if(this.y + this.w >= h) this.directiony = -this.directiony;
+        if(this.y + this.h >= h) this.directiony = -this.directiony;
         if(this.y <= 0) this.directiony = -this.directiony;
     }
 
     draw() {
         this.ctx.beginPath();
-        this.ctx.rect(this.x, this.y, this.w, this.w);
+        this.ctx.rect(this.x, this.y, this.w, this.h);
         this.ctx.fillStyle = this.fill;
         this.ctx.fill();
         this.ctx.stroke();
